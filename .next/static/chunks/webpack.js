33252ef8a63d23c143c79894042e3ba3bc22a8f8
Hentai Generator <jihad.ejjilali@gmail.com>
2025-08-06 16:36:49 +0200

    fix: résolution du conflit de routes dynamiques
    - Suppression du dossier [toolId] en conflit avec [id]
    - Uniformisation des noms de paramètres dynamiques
    - Correction de l'erreur Next.js sur les routes
    - Application maintenant accessible sur localhost:3000

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e928be06c0b05f8f"; }
+/******/ 		__webpack_require__.h = function() { return "ab124f0cfdecc4ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
